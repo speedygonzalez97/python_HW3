5239b6a8c2dbcbf02f09a7a3feb4674359995a87
Bryan Gonzalez <gonzalez.bryan9@gmail.com>
2023-10-18 19:10:44 -0400

    hw 3 notebook for python module

---
+++ homework_3_2.py
@@ -0,0 +1,7 @@
+from math import pi
+
+def square_area(length, width):
+    print(length * width)
+
+def circum_circ(radius):
+    print(2 * pi * radius)
---
+++ module.py
@@ -0,0 +1,2 @@
+def printName(name):
+    return f"Hello Mr/Ms {name}...we've been waiting for you!"
